ae0a040a73582c7be323873c1d22cbc5b441f8b5
anamariadem <57289269+anamariadem@users.noreply.github.com>
2020-03-13 13:08:54 +0200

    now

--- Graph Alg/Project 1/app_start.py
+++ Graph Alg/Project 1/app_start.py
@@ -2,7 +2,7 @@ from graph_repr import *
 from file_work import *
 from user_interface import *
 
-fg = FileGraphs("graph1m.txt")
+fg = FileGraphs("graph.txt")
 fg.create_graph()
 #list = fg.getAll()
 #print(list)
